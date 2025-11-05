db16dd2e72746485ef6863b6a98f82b683c75fa1
TheWanou21 <147034259+TheWanou21@users.noreply.github.com>
2025-11-05 11:28:47 +0100

    [GITHUB] Mise sur Github

---
+++ Projet Procédural/Assets/Components/ProceduralGeneration/0_SimpleRoomPlacement/SimpleRoomPlacement.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using VTools.Grid;
+using VTools.ScriptableObjectDatabase;
+using VTools.Utility;
+
+namespace Components.ProceduralGeneration.SimpleRoomPlacement
+{
+    [CreateAssetMenu(menuName = "Procedural Generation Method/Simple Room Placement")]
+    public class SimpleRoomPlacement : ProceduralGenerationMethod
+    {
+        [Header("Room Parameters")]
+        [SerializeField] private int _maxRooms = 10;
+        [SerializeField] private Vector2Int _roomMinSize = new(5, 5);
+        [SerializeField] private Vector2Int _roomMaxSize = new(12, 8);
+
+        protected override async UniTask ApplyGeneration(CancellationToken cancellationToken)
+        {
+            // ROOM CREATIONS
+            List<RectInt> placedRooms = new();
+            int roomsPlacedCount = 0;
+            int attempts = 0;
+
+            for (int i = 0; i < _maxSteps; i++)
+            {
+                // Check for cancellation
+                cancellationToken.ThrowIfCancellationRequested();
+
+                if (roomsPlacedCount >= _maxRooms)
+                {
+                    break;
+                }
+
+                attempts++;
+
+                // choose a random size
+                int width = RandomService.Range(_roomMinSize.x, _roomMaxSize.x + 1);
+                int lenght = RandomService.Range(_roomMinSize.y, _roomMaxSize.y + 1);
+
+                // choose random position so entire room fits into grid
+                int x = RandomService.Range(0, Grid.Width - width);
+                int y = RandomService.Range(0, Grid.Lenght - lenght);
+
+                RectInt newRoom = new RectInt(x, y, width, lenght);
+
+                if (!CanPlaceRoom(newRoom, 1))
+                    continue;
+
+                PlaceRoom(newRoom);
+                placedRooms.Add(newRoom);
+
+                roomsPlacedCount++;
+
+                await UniTask.Delay(GridGenerator.StepDelay, cancellationToken: cancellationToken);
+            }
+
+            if (roomsPlacedCount < _maxRooms)
+            {
+                Debug.LogWarning($"RoomPlacer Only placed {roomsPlacedCount}/{_maxRooms} rooms after {attempts} attempts.");
+            }
+
+            if (placedRooms.Count < 2)
+            {
+                Debug.Log("Not enough rooms to connect.");
+                return;
+            }
+
+            // CORRIDOR CREATIONS
+            for (int i = 0; i < placedRooms.Count - 1; i++)
+            {
+                // Check for cancellation
+                cancellationToken.ThrowIfCancellationRequested();
+
+                Vector2Int start = placedRooms[i].GetCenter();
+                Vector2Int end = placedRooms[i + 1].GetCenter();
+
+                CreateDogLegCorridor(start, end);
+
+                await UniTask.Delay(GridGenerator.StepDelay, cancellationToken: cancellationToken);
+            }
+
+            BuildGround();
+        }
+    }
+}
---
+++ Projet Procédural/Assets/Components/ProceduralGeneration/1_BSP/BSP_Method.cs
@@ -0,0 +1,109 @@
+using Components.ProceduralGeneration;
+using Cysharp.Threading.Tasks;
+using NUnit.Framework;
+using System.Threading;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.Rendering.VirtualTexturing;
+using UnityEngine.XR;
+using VTools.RandomService;
+using System.Collections.Generic;
+
+namespace Components.ProceduralGeneration.BSP_Method
+{
+    [CreateAssetMenu(menuName = "Procedural Generation Method / BSP Method Placement")]
+
+    public class BSP_Method : ProceduralGenerationMethod
+    {
+        [Header("Room Parameters")]
+        [SerializeField] private int _maxCuts = 4;
+        [SerializeField] private Vector2Int _roomMinSize = new(3, 3);
+        [SerializeField] private Vector2Int _roomMaxSize = new(12, 12);
+        protected override async UniTask ApplyGeneration(CancellationToken cancellationToken)
+        {
+            //Instantiations
+            RectInt gridSpace = new RectInt(0, 0, Grid.Width, Grid.Lenght);
+            Node world = new Node(gridSpace, Grid , RandomService);
+
+            List<Node> nodes = new List<Node>();
+            nodes.Add(world);
+
+            //Création d'emplacements
+            for (int i = 0; i < _maxCuts; i++)
+            {
+                world.SplitXGen(_maxCuts, world, nodes);
+            }
+
+            //Création des salles
+            foreach (Node node in nodes)
+            {
+                for (int i = 0; i < _maxSteps; i++)
+                {
+
+                    int width = node.nodeSpace.width;
+                    int lenght = node.nodeSpace.height;
+                    int x = RandomService.Range(0, node.nodeSpace.width - width);
+                    int y = RandomService.Range(0, node.nodeSpace.height - lenght);
+
+                    RectInt newRoom = new RectInt(x, y, width, lenght);
+
+                    if (CanPlaceRoom(newRoom, 1))
+                    {
+                        await UniTask.Delay(GridGenerator.StepDelay, cancellationToken: cancellationToken);
+                        PlaceRoom(newRoom);
+                        break;
+                    }
+                }
+            } 
+        }
+    }
+
+    public class Node
+    {
+        public Node Child1, Child2;
+        public RectInt nodeSpace;
+
+        private readonly VTools.Grid.Grid _grid;
+        private readonly RandomService _randomService;
+
+        public Node(RectInt nodeSpace, VTools.Grid.Grid grid, RandomService randomService)
+        {
+            this.nodeSpace = nodeSpace;
+            _grid = grid;
+            _randomService = randomService;
+        }
+        public void SplitXGen(int generations, Node node, List<Node> nodes)
+        {
+            if (generations == 0) return;
+            node.Split();
+            nodes.Add(node.Child1);
+            nodes.Add(node.Child2);
+            nodes.Remove(node);
+            SplitXGen(generations - 1, node.Child1, nodes);
+            SplitXGen(generations - 1, node.Child2, nodes);
+        }
+
+        public void Split()
+        {
+            int allowedSpace = _randomService.Range(nodeSpace.width / 4, nodeSpace.width * 3 / 4);
+            //Une chance sur 2 de couper verticalement
+            if (_randomService.Chance(0.5f))
+            {
+                RectInt firstChild = new RectInt(nodeSpace.xMin, nodeSpace.yMin, allowedSpace, nodeSpace.height); 
+                RectInt secondChild = new RectInt(nodeSpace.xMin + firstChild.xMax, nodeSpace.yMin, nodeSpace.width - allowedSpace, nodeSpace.height);
+                Child1 = new Node(firstChild, _grid,_randomService);
+                Child2 = new Node(secondChild, _grid, _randomService);
+                
+            }
+            //Sinon coupure horizontale du parent
+            else
+            {
+                RectInt firstChild = new RectInt(nodeSpace.xMin, nodeSpace.yMin, nodeSpace.width, allowedSpace);
+                RectInt secondChild = new RectInt(nodeSpace.xMin, nodeSpace.yMin + firstChild.yMax, nodeSpace.width, nodeSpace.height - allowedSpace);
+                Child1 = new Node(firstChild, _grid, _randomService);
+                Child2 = new Node(secondChild, _grid, _randomService);
+            }
+            
+        }
+    }
+}
